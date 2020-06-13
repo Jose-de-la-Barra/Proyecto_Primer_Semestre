abbf4bc1d81dfb4c80f0c4a2c07004d60cdbae61
Jose <jodelabarra@alumnos.uai.cl>
2020-06-13 19:15:41 -0400

    Agregué el salto e hice más comentarios

--- pross/COVID ESCAPE.py
+++ pross/COVID ESCAPE.py
@@ -1,10 +1,13 @@
 import arcade
 import random
 import time
+import os
+
 
 SCREE_WIDHT = 1200
 SCREE_HEIGHT = 600
 SCREE_TITLE = "COVID ESCAPE"
+
 
 # constantes para escalar sprites
 escala_personaje = 0.20
@@ -15,6 +18,9 @@ escala_guantes = 0.05
 escala_mask = 0.10
 escala_gel = 0.05
 
+# características de la fisica del juego
+JUMP_SPEED = 15
+GRAVITY = 3
 MOVEMENT_SPEED = 5
 
 
@@ -23,18 +29,21 @@ class MyGame(arcade.Window):
         super().__init__(SCREE_WIDHT, SCREE_HEIGHT, SCREE_TITLE)
         arcade.set_background_color(arcade.color.ALICE_BLUE)
 
+        file_path = os.path.dirname(os.path.abspath(__file__))
+        os.chdir(file_path)
+
         self.player_list = None  # LISTA QUE CONTIENE PERSONAJE
-        self.virus_list = None
+        self.virus_list = None  # ...
         self.pisos_list = None
         self.objetos_list = None
 
         self.player_sprite = None  # VARIABLE DEL SPRITE
         self.virus_sprite = None  # VARIABLE DEL SPRITE
-        self.pisos_sprite = None
+        self.pisos_sprite = None  # ...
         self.objetos_sprite = None
 
-        self.physics_engine = None
-        self.wall_list = None
+        self.physics_engine = None  # le damos características de la función physics_engine a nuestro objeto
+        self.wall_list = None  # creamos esta característica para más adelante poder identificar ciertos objetos que no se pueden atravesar (piso, piso flotante).
 
     def setup(self):  # inicializar las listas
         self.player_list = arcade.SpriteList()  # VA PERMITIR CONTROLAR COLISIONES/MOVIMIENTO
@@ -59,22 +68,23 @@ class MyGame(arcade.Window):
         self.virus_sprite.center_y = 105
         self.virus_list.append(self.virus_sprite)
 
-        # crear piso
+        # crear piso con un loop de la imagen
         for i in range(0, 1300, 114):
             piso = arcade.Sprite("piso.png", escala_piso)
             piso.center_x = i
             piso.center_y = 32
             self.pisos_list.append(piso)
-            self.wall_list.append(piso)
+            self.wall_list.append(piso)  # el piso no se puede atravesar
 
+        # asignar coordenadas fijas a el piso flotante
         coordenas_pisoflotante = [[600, 430], [255, 200], [945, 200], [420, 320], [780, 320], [180, 460], [1107, 450]]
         for p in coordenas_pisoflotante:
             pisoaire = arcade.Sprite("Piso flotante.png", escala_pisovolador)
             pisoaire.position = p
             self.pisos_list.append(pisoaire)
-            self.wall_list.append(pisoaire)
+            self.wall_list.append(pisoaire)  # el piso flotante no se puede atravesar
 
-        # crear objetos
+        # CREAR OBJETOS
 
         cordenadas_objetos = [[600, 480], [255, 250], [945, 250], [420, 370], [780, 370], [180, 510], [1107, 500]]
         cosas = ["alcohol gel.png", "guantes.png", "mascara.png"]
@@ -97,9 +107,14 @@ class MyGame(arcade.Window):
                 material.position = j
                 self.objetos_list.append(material)
 
-        self.physics_engine = arcade.PhysicsEngineSimple(self.player_sprite,
-                                                         self.wall_list)
+        # le agregamos gravedad a nuestro personaje sin permitirle atravesar el piso ni el piso flotante.
 
+        self.physics_engine = arcade.PhysicsEnginePlatformer(self.player_sprite, self.wall_list)
+
+    def gravedad(self):
+        self.physics_engine = arcade.PhysicsEnginePlatformer(self.player_sprite,
+                                                             self.wall_list,
+                                                             gravity_constant=GRAVITY)
 
         #random objetos
         #k=1
@@ -136,6 +151,7 @@ class MyGame(arcade.Window):
        #         n=n+1
         #        print(p)
 
+
     def on_draw(self):
         arcade.start_render()
         self.player_list.draw()
@@ -144,29 +160,23 @@ class MyGame(arcade.Window):
         self.objetos_list.draw()
         self.wall_list.draw()
 
-    def on_key_press(self, key, modifiers):
-        """Called whenever a key is pressed. """
+    def on_key_press(self, key, modifiers):  # se llama cada vez que presionamos una tecla
 
         if key == arcade.key.UP:
-            self.player_sprite.change_y = MOVEMENT_SPEED
+            if self.physics_engine.can_jump():
+                self.player_sprite.change_y = JUMP_SPEED
+
         elif key == arcade.key.LEFT:
             self.player_sprite.change_x = -MOVEMENT_SPEED
         elif key == arcade.key.RIGHT:
             self.player_sprite.change_x = MOVEMENT_SPEED
 
-    def on_key_release(self, key, modifiers):
+    def on_key_release(self, key, modifiers):  # para cuando el usuario suelta la tecla
         """Called when the user releases a key. """
-
-        if key == arcade.key.UP:
-            self.player_sprite.change_y = 0
-        elif key == arcade.key.LEFT or key == arcade.key.RIGHT:
+        if key == arcade.key.LEFT or key == arcade.key.RIGHT:
             self.player_sprite.change_x = 0
 
-    def on_update(self, delta_time):
-        """ Movement and game logic """
-
-        # Call update on all sprites (The sprites don't do much in this
-        # example though.)
+    def on_update(self, delta_time):  # Actualización
         self.physics_engine.update()
 
 
