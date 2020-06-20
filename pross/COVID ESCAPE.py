ae1db67155776dd785c9e364ac2835ef72306234
Jose <jodelabarra@alumnos.uai.cl>
2020-06-20 16:10:01 -0400

    Ahora el virus sigue a el personaje pero le falta agregarle atributos fisicos y colisión con el personaje

--- pross/COVID ESCAPE.py
+++ pross/COVID ESCAPE.py
@@ -8,7 +8,6 @@ SCREE_WIDHT = 1200
 SCREE_HEIGHT = 600
 SCREE_TITLE = "COVID ESCAPE"
 
-
 # constantes para escalar sprites
 escala_personaje = 0.9
 escala_virus = 0.15
@@ -23,13 +22,53 @@ escala_sol = 0.5
 JUMP_SPEED = 15
 GRAVITY = 3
 MOVEMENT_SPEED = 5
+VIRUS_SPEED = 2
 
 # Cuántos píxeles para mantener como margen mínimo entre el personaje
 # y el borde de la pantalla.
-#LEFT_VIEWPORT_MARGIN = 0
-#RIGHT_VIEWPORT_MARGIN = 0
-#BOTTOM_VIEWPORT_MARGIN = 0
-#TOP_VIEWPORT_MARGIN = 0
+# LEFT_VIEWPORT_MARGIN = 0
+# RIGHT_VIEWPORT_MARGIN = 0
+# BOTTOM_VIEWPORT_MARGIN = 0
+# TOP_VIEWPORT_MARGIN = 0
+
+
+#class Ventana_Ganador(arcade.View):
+#    def __init__(self):
+#        super().__init__()
+#        self.texture = "ganador.png"
+
+        # Reset the viewport, necessary if we have a scrolling game and we need
+        # to reset the viewport back to the start so we can see what we draw.
+#        arcade.set_viewport(0, SCREE_WIDHT - 1, 0, SCREE_HEIGHT - 1)
+
+#    def on_draw(self):
+#        """ Draw this view """
+#        arcade.start_render()
+#        self.texture.draw_sized(SCREE_WIDHT / 2, SCREE_HEIGHT / 2,
+#                                SCREE_WIDHT, SCREE_HEIGHT)
+
+#    def on_mouse_press(self, _x, _y, _button, _modifiers):
+#        """ If the user presses the mouse button, re-start the game. """
+#        game_view = GameView()
+#        game_view.setup()
+#        self.window.show_view(game_view)
+
+
+class Virus(arcade.Sprite):
+
+    def follow_sprite(self, player_sprite):
+        # Esta función es para que el virus (self) se mueva hacia el personaje (player.sprite).
+
+        if self.center_y < player_sprite.center_y:
+            self.center_y += min(VIRUS_SPEED, player_sprite.center_y - self.center_y)
+        elif self.center_y > player_sprite.center_y:
+            self.center_y -= min(VIRUS_SPEED, self.center_y - player_sprite.center_y)
+
+        if self.center_x < player_sprite.center_x:
+            self.center_x += min(VIRUS_SPEED, player_sprite.center_x - self.center_x)
+        elif self.center_x > player_sprite.center_x:
+            self.center_x -= min(VIRUS_SPEED, self.center_x - player_sprite.center_x)
+
 
 class MyGame(arcade.Window):
     def __init__(self):
@@ -43,69 +82,67 @@ class MyGame(arcade.Window):
         self.virus_list = None  # ...
         self.pisos_list = None
         self.objetos_list = None
-        self.decoración_list = None
+        self.decoracion_list = None
 
         self.player_sprite = None  # VARIABLE DEL SPRITE
         self.virus_sprite = None  # VARIABLE DEL SPRITE
         self.pisos_sprite = None  # ...
         self.objetos_sprite = None
-        self.decoración_sprite = None
+        self.decoracion_sprite = None
 
         self.physics_engine = None  # le damos características de la función physics_engine a nuestro objeto
         self.wall_list = None  # creamos esta característica para más adelante poder identificar ciertos objetos que no se pueden atravesar (piso, piso flotante).
 
-        self.collect_objetos_sound = arcade.load_sound("Recoger.mp3") #Sonido cuando toma cosas el personaje
-        self.jump_sound = arcade.load_sound("salto.mp3") #Efecto de sonido cuando salta el personaje
+        self.collect_objetos_sound = arcade.load_sound("Recoger.mp3")  # Sonido cuando toma cosas el personaje
+        self.jump_sound = arcade.load_sound("salto.mp3")  # Efecto de sonido cuando salta el personaje
 
         # Se utiliza para realizar un seguimiento de nuestro desplazamiento
         self.view_bottom = 0
         self.view_left = 0
 
-        self.score = 0 #Lleva un registro de la puntuación
+        self.score = 0  # Lleva un registro de la puntuaciónç
 
     def setup(self):  # inicializar las listas
         self.player_list = arcade.SpriteList()  # VA PERMITIR CONTROLAR COLISIONES/MOVIMIENTO
         self.virus_list = arcade.SpriteList()
         self.pisos_list = arcade.SpriteList()
         self.objetos_list = arcade.SpriteList()
-        self.decoración_list = arcade.SpriteList()
+        self.decoracion_list = arcade.SpriteList()
 
 #        self.player_list.append(self.player_sprite)
         self.wall_list = arcade.SpriteList()
-        #AMBIENTE
+        # AMBIENTE
         sol = "sun1.png"
-        self.decoración_sprite = arcade.Sprite(sol, escala_sol)
-        self.decoración_sprite.center_x = 74
-        self.decoración_sprite.center_y = 560
-        self.decoración_list.append(self.decoración_sprite)
+        self.decoracion_sprite = arcade.Sprite(sol, escala_sol)
+        self.decoracion_sprite.center_x = 74
+        self.decoracion_sprite.center_y = 560
+        self.decoracion_list.append(self.decoracion_sprite)
 
         letrero = "sign.png"
-        self.decoración_sprite = arcade.Sprite(letrero, 0.7)
-        self.decoración_sprite.center_x = 670
-        self.decoración_sprite.center_y = 108
-        self.decoración_list.append(self.decoración_sprite)
+        self.decoracion_sprite = arcade.Sprite(letrero, 0.7)
+        self.decoracion_sprite.center_x = 670
+        self.decoracion_sprite.center_y = 108
+        self.decoracion_list.append(self.decoracion_sprite)
 
-        piedra_x = [200,690]
+        piedra_x = [200, 690]
         for k in range(len(piedra_x)):
             piedra = "rock.png"
-            self.decoración_sprite = arcade.Sprite(piedra, 0.3)
-            self.decoración_sprite.center_x = piedra_x[k]
-            self.decoración_sprite.center_y = 80
-            self.decoración_list.append(self.decoración_sprite)
+            self.decoracion_sprite = arcade.Sprite(piedra, 0.3)
+            self.decoracion_sprite.center_x = piedra_x[k]
+            self.decoracion_sprite.center_y = 80
+            self.decoracion_list.append(self.decoracion_sprite)
 
         # Se utiliza para realizar un seguimiento de nuestro desplazamiento
         self.view_bottom = 0
         self.view_left = 0
-        #Puntuación
-        self.score = 0 #Lleva un registro de la puntuación
+        # Puntuación
+        self.score = 0  # Lleva un registro de la puntuación
 
-
-        for i in range (0,1200,150):
+        for i in range(0, 1200, 150):
             pasto = arcade.Sprite("grass.png", escala_sol)
             pasto.center_x = i
             pasto.center_y = 93
-            self.decoración_list.append(pasto)
-
+            self.decoracion_list.append(pasto)
 
         # Crear personaje
         personaje = "adventurer_swim1.png"
@@ -115,11 +152,10 @@ class MyGame(arcade.Window):
         self.player_list.append(self.player_sprite)
 
         # CREAR VIRUS
-        virus = "virus.png"
-        self.virus_sprite = arcade.Sprite(virus, escala_virus)
-        self.virus_sprite.center_x = 1107
-        self.virus_sprite.center_y = 105
-        self.virus_list.append(self.virus_sprite)
+        virus = Virus("virus.png", escala_virus)  # hereda las características de la clase Virus
+        virus.center_x = 1107
+        virus.center_y = 105
+        self.virus_list.append(virus)
 
         # crear piso con un loop de la imagen
         for i in range(0, 1300, 64):
@@ -130,7 +166,8 @@ class MyGame(arcade.Window):
             self.wall_list.append(piso)  # el piso no se puede atravesar
 
         # asignar coordenadas fijas a el piso flotante
-        coordenas_pisoflotante = [[64,320],[600,200],[520, 430],[650,430], [930,430],[255, 200], [945, 200], [400, 320], [780, 320],[1107, 310], [180, 460], [1107, 450]]
+        coordenas_pisoflotante = [[64, 320], [600, 200], [520, 430], [650, 430], [930, 430], [255, 200],
+                                  [945, 200], [400, 320], [780, 320], [1107, 310], [180, 460], [1107, 450]]
         n = 1
         cosas = ["alcohol gel.png", "guantes.png", "mascara.png"]
         cte_eje_y = 50
@@ -138,35 +175,28 @@ class MyGame(arcade.Window):
             coordenas__choicepisoflotante = random.choice(coordenas_pisoflotante)
             coordenas_pisoflotante.remove(coordenas__choicepisoflotante)
 
-            cordenadas_lista=[coordenas__choicepisoflotante]
+            cordenadas_lista = [coordenas__choicepisoflotante]
 
             for p in cordenadas_lista:
-                pisoaire=arcade.Sprite("ground_grass_small_broken.png",escala_pisovolador)
-                pisoaire.position=p
+                pisoaire = arcade.Sprite("ground_grass_small_broken.png", escala_pisovolador)
+                pisoaire.position = p
                 self.pisos_list.append(pisoaire)
                 self.wall_list.append(pisoaire)  # el piso flotante no se puede atravesar
-                n=n+1
-
+                n += 1
 
                 o = random.choice(cosas)
-
                 if o == "alcohol gel.png":
                     material = arcade.Sprite(o, escala_gel)
-                    material.position = p[0],p[1]+cte_eje_y
+                    material.position = p[0], p[1]+cte_eje_y
                     self.objetos_list.append(material)
-
                 elif o == "guantes.png":
                     material = arcade.Sprite(o, escala_guantes)
-                    material.position = p[0],p[1]+cte_eje_y
+                    material.position = p[0], p[1]+cte_eje_y
                     self.objetos_list.append(material)
-
                 elif o == "mascara.png":
                     material = arcade.Sprite(o, escala_mask)
-                    material.position = p[0],p[1]+cte_eje_y
+                    material.position = p[0], p[1]+cte_eje_y
                     self.objetos_list.append(material)
-
-
-
 
        # coordenas_pisoflotante = [[600, 430], [255, 200], [945, 200], [420, 320], [780, 320], [180, 460], [1107, 450]]
         #for p in coordenas_pisoflotante:
@@ -174,8 +204,6 @@ class MyGame(arcade.Window):
           #  pisoaire.position = p
            # self.pisos_list.append(pisoaire)
             #self.wall_list.append(pisoaire)  # el piso flotante no se puede atravesar
-
-
 
         # le agregamos gravedad a nuestro personaje sin permitirle atravesar el piso ni el piso flotante.
         self.physics_engine = arcade.PhysicsEnginePlatformer(self.player_sprite, self.wall_list)
@@ -187,8 +215,8 @@ class MyGame(arcade.Window):
 
     def on_draw(self):
         arcade.start_render()
-        self.decoración_list.draw()
-        arcade.draw_text("PELIGRO!\nCOVID-19",670,111,arcade.color.DARK_CANDY_APPLE_RED,15, width=100, align="center",
+        self.decoracion_list.draw()
+        arcade.draw_text("PELIGRO!\nCOVID-19", 670, 111, arcade.color.DARK_CANDY_APPLE_RED, 15, width=100, align="center",
                          anchor_x="center", anchor_y="center")
         self.player_list.draw()
         self.virus_list.draw()
@@ -196,17 +224,16 @@ class MyGame(arcade.Window):
         self.objetos_list.draw()
         self.wall_list.draw()
 
-        #Dibuja nuestro puntaje en la pantalla, desplazándolo con la ventana gráfica
+        # Dibuja nuestro puntaje en la pantalla, desplazándolo con la ventana gráfica
         score_text = f"Score: {self.score}"
-        arcade.draw_text(score_text, 10 , 10 , arcade.csscolor.WHITE, 18)
-
+        arcade.draw_text(score_text, 10, 10, arcade.csscolor.WHITE, 18)
 
     def on_key_press(self, key, modifiers):  # se llama cada vez que presionamos una tecla
 
         if key == arcade.key.UP:
             if self.physics_engine.can_jump():
                 self.player_sprite.change_y = JUMP_SPEED
-                arcade.play_sound(self.jump_sound)#Efecto sonido de salto cuando se presiona la tecla "UP"
+                arcade.play_sound(self.jump_sound)  # Efecto sonido de salto cuando se presiona la tecla "UP"
         elif key == arcade.key.LEFT:
             self.player_sprite.change_x = -MOVEMENT_SPEED
         elif key == arcade.key.RIGHT:
@@ -220,15 +247,23 @@ class MyGame(arcade.Window):
     def on_update(self, delta_time):  # Actualización
         self.physics_engine.update()
 
-        #Mira si golpeamos algun objeto
-        objetos_hit_list = arcade.check_for_collision_with_list(self.player_sprite,self.objetos_list)
-        #Recorre cada objeto que golpeamos (si hay alguno) y lo retira el objeto de  para monedas en objetos_hit_list:
+        # Mira si golpeamos algun objeto
+        objetos_hit_list = arcade.check_for_collision_with_list(self.player_sprite, self.objetos_list)
+        # Recorre cada objeto que golpeamos (si hay alguno) y lo retira el objeto de  para monedas en objetos_hit_list:
         for objetos in objetos_hit_list:
             # Remueve el objeto
             objetos.remove_from_sprite_lists()
             # Hace un sonido al "tomar" el objeto
             arcade.play_sound(self.collect_objetos_sound)
-            self.score += 1 #Agrega uno al puntaje
+            self.score += 1  # Agrega uno al puntaje
+
+        for virus in self.virus_list:
+            virus.follow_sprite(self.player_sprite)
+
+
+#        if self.score == 9:
+#            view = Ventana_Ganador()
+#            self.window.show_view(view)
 
         # --- Administrar desplazamiento ---
 
